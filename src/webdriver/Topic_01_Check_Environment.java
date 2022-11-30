fdfaa10b742a2a80e9215ad363b60178392a11ae
tranghuyendtht <trangdth202@gmail.com>
2022-11-30 09:48:25 +0700

    add cmt

--- src/webdriver/Topic_01_Check_Environment.java
+++ src/webdriver/Topic_01_Check_Environment.java
@@ -27,6 +27,7 @@ public class Topic_01_Check_Environment {
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		driver.get("https://www.facebook.com/");
+		//cmt
 	}
 
 	@Test
